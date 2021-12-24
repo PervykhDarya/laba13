d6c5dfb27472ffa3dfb7e90ef54953e179088ddf
PervykhDarya <dashkapervykh@mail.ru>
2021-12-24 17:24:20 +0300

    добавлены примеры

---
+++ PyCharm/primer1.py
@@ -0,0 +1,7 @@
+def print_these(a, b, c):
+    print(a, "is stored in a")
+    print(b, "is stored in b")
+    print(c, "is stored in c")
+
+
+print_these(1,2,3)
---
+++ PyCharm/primer4.py
@@ -0,0 +1,7 @@
+def print_these(a=None, b=None, c=None):
+    print(a, "is stored in a")
+    print(b, "is stored in b")
+    print(c, "is stored in c")
+
+
+print_these(c=3, a=1)
